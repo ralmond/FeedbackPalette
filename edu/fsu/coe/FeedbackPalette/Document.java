2cc7d1565d87db7e2cf5b29c8f2ddb4b9e3e5d55
Russell Almond <almond@acm.org>
2023-02-03 11:46:05 -0500

    Added Keywords to document.  Still need to think about keyword iterators and visibility of set operatior.

--- edu/fsu/coe/FeedbackPalette/Document.java
+++ edu/fsu/coe/FeedbackPalette/Document.java
@@ -1,6 +1,7 @@
 package edu.fsu.coe.FeedbackPalette;
 
 
+import java.util.*;
 
 /**
  * Class Document
@@ -10,12 +11,18 @@ public class Document {
   //
   // Fields
   //
-
+  /**
+   * Set of keywords for the document.
+   */
+  SortedSet<String> keywords= NULL;
   
   //
   // Constructors
   //
-  public Document () { };
+  public Document () { 
+    keywords = new TreeSet<String>();
+    
+    };
   
   //
   // Methods
@@ -25,9 +32,26 @@ public class Document {
   //
   // Accessor methods
   //
+  public SortedSet<String> getKeywords(){
+      return(keywords);
+  }
 
   //
   // Other methods
   //
+  
+  public boolean addKeyword(String newKeyword) {
+    return(keywords.add(newKeyword));
+  }
+  public void clearKeywords() {
+    keywords.clear();
+  }
+  public boolean hasKeyword(String key){
 
+      return(keywords.contains(key));
+  }
+  public boolean removeKeyword(String key) {
+      keywords.remove(key);
+  }
+// add, addAll, clear, contains, containsAll, equals, hashCode, isEmpty, iterator, remove, removeAll, retainAll, size, toArray, toArray
 }
