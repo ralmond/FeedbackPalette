81e7001666f868a5be314234e110bebd8fc5260d
James Hudson IV <76814998+JLH1138@users.noreply.github.com>
2023-03-27 14:59:51 -0400

    use of class "notereader"

--- edu/fsu/coe/FeedbackPalette/notereader.java
+++ edu/fsu/coe/FeedbackPalette/notereader.java
@@ -7,15 +7,35 @@ package edu.fsu.coe.FeedbackPalette;
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+
 public class notereader
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    public static void main(String[] args) {
+    try {
+      File myObj = new File("EDF_4210FeedbackDatabase_-_Notes.tsv");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+    
+    /*// instance variables - replace the example below with your own
+    *private int x;
 
     /**
      * Constructor for objects of class notereader
      */
-    public notereader()
+    /*public notereader()
     {
         // initialise instance variables
         x = 0;
@@ -27,9 +47,10 @@ public class notereader
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    /*public int sampleMethod(int y)
     {
         // put your code here
         return x + y;
     }
+    */
 }
